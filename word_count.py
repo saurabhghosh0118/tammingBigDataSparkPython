33d58ab6d56ad3cd4ac9f5b6826dcbc6713d4ccf
saurabhghosh0118 <saurabhghosh1801@gmail.com>
2025-04-07 13:17:00 +0530

    test

--- word_count.py
+++ word_count.py
@@ -8,5 +8,5 @@ spark = SparkSession.builder.config(conf=conf).getOrCreate()
 rdd1 = sc.textFile(r"D:\pythonProject\tammingBigDataSparkPython\Book")
 rdd2 = rdd1.flatMap(lambda x: x.split(" ")).map(lambda x: (x,1)).reduceByKey(lambda x,y: (x+y))
 df2 = spark.createDataFrame(rdd2)
-df2.show(10)
+df2.show(20)
 #This is to test merg of main to branch using pycharm
