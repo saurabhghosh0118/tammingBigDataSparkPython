4d6360b949f2ab7e85f04c8b64063ee0bc0c8b50
saurabhghosh0118 <saurabhghosh1801@gmail.com>
2025-04-07 13:14:06 +0530

    test

--- word_count.py
+++ word_count.py
@@ -8,4 +8,5 @@ spark = SparkSession.builder.config(conf=conf).getOrCreate()
 rdd1 = sc.textFile(r"D:\pythonProject\tammingBigDataSparkPython\Book")
 rdd2 = rdd1.flatMap(lambda x: x.split(" ")).map(lambda x: (x,1)).reduceByKey(lambda x,y: (x+y))
 df2 = spark.createDataFrame(rdd2)
-df2.show(10)
+df2.show(20)
+#Tis is my code
