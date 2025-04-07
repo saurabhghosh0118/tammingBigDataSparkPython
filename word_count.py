72ea96dc9c7c6ea3689fe581062d4aa8446aa768
saurabhghosh0118 <saurabhghosh1801@gmail.com>
2025-04-08 02:54:30 +0530

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -9,8 +9,7 @@ rdd1 = sc.textFile(r"D:\pythonProject\tammingBigDataSparkPython\Book")
 rdd2 = rdd1.flatMap(lambda x: x.split(" ")).map(lambda x: (x,1)).reduceByKey(lambda x,y: (x+y))
 df2 = spark.createDataFrame(rdd2)
 df2.show(20)
-#This comment is written in test branch
-#This is my local branch
+#This comment is written in test branch - update
 #This is my change from test branch - line1
 #This is my change from test branch - line2
 
