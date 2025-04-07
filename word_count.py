1fbeb74a495cf5f9e0e61d1fae4f27a23c50e6e0
saurabhghosh0118 <saurabhghosh1801@gmail.com>
2025-04-07 15:29:02 +0530

    test2

--- word_count.py
+++ word_count.py
@@ -9,6 +9,7 @@ rdd1 = sc.textFile(r"D:\pythonProject\tammingBigDataSparkPython\Book")
 rdd2 = rdd1.flatMap(lambda x: x.split(" ")).map(lambda x: (x,1)).reduceByKey(lambda x,y: (x+y))
 df2 = spark.createDataFrame(rdd2)
 df2.show(20)
+#This comment is written in test branch
 #This is to test merg of main to branch using pycharm
 #Second line from main branch
 #Third Line from main
